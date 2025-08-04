9f66790c54e51bdf02b9eb1d5bc5097ffd20dc75
Denis <dddenisss35@gmail.com>
2025-08-04 13:46:25 +0300

    Refactoring

---
+++ game_framework_pro/game_framework_pro/src/game_framework_pro.rs
@@ -0,0 +1,10 @@
+//pub mod _00_program;
+//pub mod _01_ui;
+//pub mod _02_app;
+//pub mod _03_game;
+//pub mod tests_00;
+//
+//pub use _00_program as program;
+//pub use _01_ui as ui;
+//pub use _02_app as app;
+//pub use _03_game as game;
