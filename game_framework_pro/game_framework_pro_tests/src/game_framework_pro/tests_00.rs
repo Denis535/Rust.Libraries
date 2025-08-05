d3343223086296649c8b8341f6797be0995ec97f
Denis <dddenisss35@gmail.com>
2025-08-05 10:06:01 +0300

    ProgramBase

--- game_framework_pro/game_framework_pro_tests/src/game_framework_pro/tests_00.rs
+++ game_framework_pro/game_framework_pro_tests/src/game_framework_pro/tests_00.rs
@@ -1,7 +1,8 @@
 #![cfg(test)]
+use crate::std::boxed::*;
 use crate::game_framework_pro::program::*;
 
 #[test]
 fn test_00() {
-    let _program = Program::new();
+    let _program = Box::new(Program::new());
 }
